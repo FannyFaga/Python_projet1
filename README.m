78d7b1546853cd80aa5679d07e68cfdae8ce309e
Faga Ibrahima Fanny <137840301+FannyFaga@users.noreply.github.com>
2025-01-24 19:09:39 +0000

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,7 @@
-# Python_projet1
+MAC Address Changer
+This project is a Python script designed for cybersecurity enthusiasts and network administrators. 
+It allows users to easily change the MAC address of a specified network interface. 
+This tool can be used for various purposes, including:
+Enhancing network privacy by spoofing the MAC address.
+Bypassing certain network restrictions or access controls.
+Testing and learning about network security.
