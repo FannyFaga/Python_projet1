322aeb60a5a88a8e7dc5d172c8d033707aa102d3
FannyFaga <levisfanny33@gmail.com>
2025-01-25 01:26:28 +0100

    final version

--- mac-changing.py
+++ mac-changing.py
@@ -4,16 +4,12 @@ import subprocess
 import optparse
 
 def search_mac_address(mac_address):
-    """
-    Searches for a valid MAC address pattern in the provided string.
-    """
+
     match = re.search(r'(([a-fA-F0-9]{2}[:\-]){5}[a-fA-F0-9]{2})', mac_address)
     return match.group(0) if match else None
 
 def get_arguments():
-    """
-    Parses command-line arguments for interface and new MAC address.
-    """
+    
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
@@ -27,18 +23,14 @@ def get_arguments():
     return options
 
 def set_new_mac_address(interface, new_mac):
-    """
-    Sets a new MAC address for the given interface.
-    """
+  
     print(f"Changing MAC address for {interface} to {new_mac}")
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
 def get_mac_address(interface):
-    """
-    Retrieves the current MAC address of the specified interface.
-    """
+   
     try:
         ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode("utf-8")
         return search_mac_address(ifconfig_result)
